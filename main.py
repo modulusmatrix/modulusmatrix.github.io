bf7b02520aeb39fcd3d16703715c228fdc881123
modulusmatrix <yazarbayramgedikli@gmail.com>
2026-06-21 10:43:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-def calculate(M, S, E, X, T):
-    Wm, Ws, We, Wx, Wt = 0.25, 0.2, 0.2, 0.2, 0.15
+import random
 
-    score = (
-        Wm * M +
-        Ws * S +
-        We * E -
-        Wx * X +
-        Wt * T
+def calculate(M, S, E, X, T):
+    base = (
+        0.3*M +
+        0.2*S +
+        0.2*E -
+        0.2*X +
+        0.1*T
     )
 
-    return round(score, 3)
+    noise = random.uniform(-0.15, 0.15)
 
-event = "test event"
-M, S, E, X, T = 0.4, 0.7, 0.6, 0.3, 0.8
-
-score = calculate(M,S,E,X,T)
-
-print("EVENT:", event)
-print("SCORE:", score)
-print("CONFIDENCE:", round(1-X,3))
-M, S, E, X, T = 0.9, 0.2, 0.8, 0.1, 0.7
+    return round(base + noise, 3)
